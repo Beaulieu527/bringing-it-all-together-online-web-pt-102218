9ef98af87f663915c4f232e22f62161efff2382b
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-03 22:36:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,16 +51,17 @@ class Dog
     new(id: row[0], name: row[1], breed: row[2])
   end
 
-  def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-    if dog
+  def self.find_or_create_by(name:, album:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND album = ?", name, album)
+    if !dog.empty?
       dog_data = dog[0]
-      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = self.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      dog = self.create(name: name, breed: breed)
+      dog = self.create(name: name, album: album)
     end
     dog
   end
+
 
   def update
     sql = <<-SQL
